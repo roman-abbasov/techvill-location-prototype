e5594b2116602d56306133e76a5a747004b2f9df
Roman Abbasov <balsagoth09@gmail.com>
2026-07-13 17:14:45 +0300

    ci: deploy site to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/techvill-location-prototype/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
